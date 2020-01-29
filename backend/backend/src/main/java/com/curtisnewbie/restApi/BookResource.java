df4749f9c3300bdd884e1a6a892740be35cb7246
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-29 21:30:29 +0000

    Add BookResource class that manages RESTful endpoints
    RESTful endpoint methods in this class are not yet been completely implemented. Methods are implemented in such a way to mimic the actual behaviours and will be updated to interact with the database.
    
    modified:   backend/backend/.classpath
    modified:   backend/backend/.settings/org.eclipse.core.resources.prefs
    new file:   backend/backend/src/main/java/com/curtisnewbie/restApi/BookResource.java

---
+++ backend/backend/src/main/java/com/curtisnewbie/restApi/BookResource.java
@@ -0,0 +1,85 @@
+package com.curtisnewbie.restApi;
+
+import javax.ejb.EJB;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.*;
+
+import com.curtisnewbie.model.*;
+import com.curtisnewbie.util.*;
+
+/**
+ * RESTful api for Book Resources
+ */
+@Path("book")
+public class BookResource {
+
+    @EJB
+    BookRepository dbConn;
+
+    // http://localhost:8080/api/book?id=123-456
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getBookById(@QueryParam("id") String id) {
+        var book = new Book();
+        book.setTitle("Curtis");
+        book.setId("123-456");
+        if (id.equals(book.getId()))
+            return Response.ok(book).build();
+        else
+            return Response.noContent().build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createBook(Book book) {
+        // for testing
+        String id;
+        if ((id = book.getId()) != null && !id.isEmpty()) {
+            dbConn.createBook(book);
+            return Response.created(
+                    UriBuilder.fromPath("http://localhost:8080/api/book/").queryParam("id", book.getId()).build())
+                    .build();
+        } else {
+            return Response.noContent().build();
+        }
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateBook(Book book) {
+        // for testing
+        String id;
+        if ((id = book.getId()) != null && id.equals("123-456")) {
+            book.setTitle("put updated");
+            return Response.ok(book).build();
+        } else {
+            return Response.noContent().build();
+        }
+    }
+
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteBook(@QueryParam("id") String id) {
+        // for testing
+        if (id != null && !id.isEmpty())
+            return Response.ok("Book id:" + id + " deleted").build();
+        else
+            return Response.noContent().build();
+    }
+
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllBooks() {
+        var books = dbConn.getBooks();
+        return Response.ok(books).build();
+    }
+}
