d6adc0b9f36e0f9e8f51ba89715ce12a91aa2c3c
CurtisNewbie <zhuangyongj@gmail.com>
2020-01-29 14:04:35 +0000

    Add Application configuring the root path for RESTful api
    I.e., @ApplicationPath("api")
    
    new file:   backend/backend/src/main/java/com/curtisnewbie/restApi/ApplicationConfig.java

---
+++ backend/backend/src/main/java/com/curtisnewbie/restApi/ApplicationConfig.java
@@ -0,0 +1,9 @@
+package com.curtisnewbie.restApi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/** Root path of RESTful api */
+@ApplicationPath("api")
+public class ApplicationConfig extends Application {
+}
