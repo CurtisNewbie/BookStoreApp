a9017fcfdcc1861576e33f3ffa1f74f822ad9b27
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-02 18:52:36 +0000

    Add Order model class
    Order class is an Entity class in JPA, wherein the Address class is "embedded" in this class as part of it. Order class should also be related to Book, i.e., an order has one or many books. However, this is not implemented yet.
    
    new file:   backend/src/main/java/com/curtisnewbie/model/Order.java

---
+++ backend/backend/src/main/java/com/curtisnewbie/model/Order.java
@@ -0,0 +1,116 @@
+package com.curtisnewbie.model;
+
+import java.util.Date;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/** Representation of Order */
+@Entity
+@Table(name = "BookStore_Order")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long orderId;
+
+    @NotNull
+    private String firstName;
+
+    @NotNull
+    private String lastName;
+
+    @NotNull
+    private Date date;
+
+    @NotNull
+    @Embedded
+    private Address address;
+
+    /*
+     * 
+     * ----------------------------------------------------
+     * 
+     * 
+     * Relation With Book is not yet implemented!!!!
+     * 
+     * 
+     * ----------------------------------------------------
+     * 
+     */
+
+    /**
+     * @return the orderId
+     */
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    /**
+     * @param orderId the orderId to set
+     */
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the address
+     */
+    public Address getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+}
