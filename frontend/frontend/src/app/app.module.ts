2b9163c937da061e57756d276bafaca782b1b5da
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-04 22:09:58 +0000

    Impl book-list component that displays a list of books
    This component will be displaying a list of books for sale in the bookstore.
    This component itself may be a way to navigate to the component where the
    details of each book is displayed.

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, HomePageComponent],
+  declarations: [AppComponent, NavBarComponent, HomePageComponent, BookListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/frontend/src/app/book-list/book-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from "@angular/core";
+import { Book } from "../model/book";
+
+@Component({
+  selector: "app-book-list",
+  templateUrl: "./book-list.component.html",
+  styleUrls: ["./book-list.component.css"]
+})
+export class BookListComponent implements OnInit {
+  books: Book[];
+
+  constructor() {}
+
+  ngOnInit() {
+    this.fetchBooks();
+  }
+
+  fetchBooks() {
+    this.books = [
+      new Book(
+        "123-456",
+        "Learning JavaEE In 10mins",
+        "Somebody",
+        "it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, ",
+        new Date()
+      ),
+      new Book(
+        "456-123",
+        "Learning JS In 10mins",
+        "NoBody",
+        "it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, ",
+        new Date()
+      )
+    ];
+  }
+}
