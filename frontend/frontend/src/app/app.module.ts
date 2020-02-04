9917048231a85e80f854cf8dc1d689f728d37b9d
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-04 21:18:34 +0000

    Impl home-page component as the view of the "Home Page"
    This component will be the home page of this app. It is mainly responsible
    for displaying news, or any other relavant information about the book store.

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { HomePageComponent } from "./home-page/home-page.component";
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent],
+  declarations: [AppComponent, NavBarComponent, HomePageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/frontend/src/app/home-page/home-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from "@angular/core";
+import { HomeNew } from "../home-new";
+
+@Component({
+  selector: "app-home-page",
+  templateUrl: "./home-page.component.html",
+  styleUrls: ["./home-page.component.css"]
+})
+export class HomePageComponent implements OnInit {
+  news: HomeNew[];
+  constructor() {}
+
+  ngOnInit() {
+    this.fetchNews();
+  }
+
+  /*
+  ----------------------------------
+
+  To be Implemented
+  
+  ----------------------------------
+  */
+  fetchNews() {
+    // dummy data
+    this.news = [
+      new HomeNew(
+        "Sale!!!!",
+        "Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!Today is a good day, we are gonna provide 100% discount, everything is for free!!!!!!! Pog!!!!",
+        new Date()
+      ),
+      new HomeNew(
+        "Welcome To the most loved Bookstore ever",
+        "Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!Look around and don't buy anything!!!!",
+        new Date()
+      )
+    ];
+  }
+}
