5b1b5e5549a6c939d00ea8bb6eeb9619d0b6d2c3
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-05 21:29:08 +0000

    Impl book-detail component that displays details of a selected book
    In Book-list component, when user selects a book, this book-detail component is displayed to show the details of a book. After viewing the details of a book, the user is able add this book to its buscket or to checkout.

--- frontend/frontend/src/app/app.module.ts
+++ frontend/frontend/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from "./app.component";
 import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { BookListComponent } from './book-list/book-list.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, HomePageComponent, BookListComponent],
+  declarations: [AppComponent, NavBarComponent, HomePageComponent, BookListComponent, BookDetailComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/frontend/src/app/book-detail/book-detail.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from "@angular/core";
+import { Book } from "../model/book";
+
+@Component({
+  selector: "app-book-detail",
+  templateUrl: "./book-detail.component.html",
+  styleUrls: ["./book-detail.component.css"]
+})
+export class BookDetailComponent implements OnInit {
+  book: Book;
+  constructor() {}
+
+  ngOnInit() {
+    this.fetchHero();
+  }
+
+  fetchHero() {
+    this.book = new Book(
+      "123-456",
+      "Learning JavaEE In 10mins",
+      "Somebody",
+      "it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, it is just impossible to learn anything in 10 minutes, ",
+      new Date()
+    );
+  }
+}
