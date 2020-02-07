0282161c5541d5f6c3bf08c6fe3f2073668a5926
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-07 15:26:03 +0000

    Impl RESTful API for HomeNew resources
    This class implements the RESTful endpoints for HomeNew entity/resources.

---
+++ backend/backend/src/main/java/com/curtisnewbie/restApi/HomeNewResource.java
@@ -0,0 +1,71 @@
+package com.curtisnewbie.restApi;
+
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+
+import com.curtisnewbie.model.HomeNew;
+import com.curtisnewbie.util.HomeNewRepository;
+
+@Path("new")
+public class HomeNewResource {
+
+    @Inject
+    private HomeNewRepository homeNewRepo;
+
+    @GET
+    @Path("all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllHomeNew() {
+        var list = homeNewRepo.getHomeNews();
+        return Response.ok(list).build();
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getHomeNewById(@QueryParam("id") long id) {
+        var n = homeNewRepo.getHomeNewById(id);
+        if (n != null)
+            return Response.ok(n).build();
+        else
+            return Response.noContent().build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response updateHomeNew(@NotNull HomeNew homeNew) {
+        if (homeNewRepo.updateHomeNew(homeNew))
+            return Response.ok(homeNew).build();
+        else
+            return Response.noContent().build();
+    }
+
+    @DELETE
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteHomeNewById(@QueryParam("id") long id) {
+        if (homeNewRepo.removeHomeNewById(id))
+            return Response.ok("HomeNew id: " + id + " deleted.").build();
+        else
+            return Response.noContent().build();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response createHomeNew(@NotNull HomeNew homeNew) {
+        homeNew = homeNewRepo.createHomeNew(homeNew);
+        var id = homeNew.getId();
+        return Response.ok(UriBuilder.fromPath("http://localhost:8080/api/new").queryParam("id", id).build()).build();
+    }
+}
