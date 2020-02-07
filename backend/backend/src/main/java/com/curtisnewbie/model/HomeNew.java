f9d942588a1757d2044f950b87df4acd80f56858
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-07 14:06:17 +0000

    Add JPA Entity "HomeNew" which represents news shown in home page.
    HomeNew.java is implemented as an Entity in JPA, which will be exposed
    as a resource via RESTful API to the frontend clients.

---
+++ backend/backend/src/main/java/com/curtisnewbie/model/HomeNew.java
@@ -0,0 +1,85 @@
+package com.curtisnewbie.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class HomeNew {
+
+    @Id
+    private long id;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private Date date;
+
+    /**
+    * 
+    */
+    public HomeNew() {
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the content
+     */
+    public String getContent() {
+        return content;
+    }
+
+    /**
+     * @param content the content to set
+     */
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    /**
+     * @return the date
+     */
+    public Date getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
