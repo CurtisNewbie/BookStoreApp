2dbf072fefcddf663c2e5bdea87af55f5e25c169
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-07 21:37:48 +0000

    Impl CORS for all the HTTP responses
    A ContainerResponseFilter is implmeneted to wrap the HTTP responses, wherein
    it adds several response headers to enbale CORS. CORS is a security policy
    implemented in client side browser. By enabling CORS and setting the origin
    manually to localhost:4200, the angualr can now access to the response sent
    by this backend REST web services.

---
+++ backend/backend/src/main/java/com/curtisnewbie/restApi/CorsFilter.java
@@ -0,0 +1,23 @@
+package com.curtisnewbie.restApi;
+
+import java.io.IOException;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+
+/** To enable CORS in development environment */
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {
+        var headers = responseContext.getHeaders();
+        headers.add("Access-Control-Allow-Origin", "http://localhost:4200");
+        headers.add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+    }
+
+}
