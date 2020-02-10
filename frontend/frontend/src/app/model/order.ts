4ad791fbe64ab1455988d5602689c3c29045b830
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-10 00:30:47 +0000

    Update Order interface to model the JSON object that is sent to backend

--- frontend/frontend/src/app/model/order.ts
+++ frontend/frontend/src/app/model/order.ts
@@ -3,7 +3,7 @@ import { Address } from "./address";
 export interface Order {
   address: Address;
   // here we only need id of each book
-  booksOnOrder: { id: string }[];
+  booksOnOrder: { amount: number; book: { id: string } }[];
   firstName: string;
   lastName: string;
 }
