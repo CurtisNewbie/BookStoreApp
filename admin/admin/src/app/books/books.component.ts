225a0b13d09e2eb8a9dfd65c69463abd4e8be0dd
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-18 18:29:43 +0000

    Imple All CRUD operations for books.component
    Admin must have JWT retrieved in order to undertake CREATE, UPDATE, and
    DELETE operation. However, they are permitted to browse books just like
    normal users. CRUD operations or the subscription for
    observables are implemented.

--- admin/admin/src/app/books/books.component.ts
+++ admin/admin/src/app/books/books.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { Book } from "../model/book";
+import { BooksService } from "../books.service";
+import { JWTAuthService } from "../jwt-auth.service";
+import { Router } from "@angular/router";
+import { HttpResponse } from "@angular/common/http";
 
 @Component({
   selector: "app-books",
@@ -7,63 +11,99 @@ import { Book } from "../model/book";
   styleUrls: ["./books.component.css"]
 })
 export class BooksComponent implements OnInit {
-  // demo data
-  books: Book[] = [
-    {
-      id: "1",
-      title: "The Animator's Survival Kit",
-      author: "Richard E. Williams",
-      content:
-        "In this book, based on his sold-out Animation Masterclass in the United States and across Europe, Williams provides the underlying principles of animation that very animator - from beginner to expert, classic animator to computer animation whiz - needs.",
-      price: 21.95,
-      date: "2009-11-05",
-      img:
-        "https://images-na.ssl-images-amazon.com/images/I/51YvgAKQYRL._SX421_BO1,204,203,200_.jpg"
-    },
-    {
-      id: "3",
-      title: "Storyboard Notebook 4:3 Panels",
-      author: "Simple Storyboards",
-      content:
-        "By the end of the storyboarding notebook you will be able to go through your storyboard writing examples and track your progress and you will have a record of what works best for you.",
-      price: 6.78,
-      date: "2020-02-01",
-      img:
-        "https://images-na.ssl-images-amazon.com/images/I/51vJRPD1sxL._SX385_BO1,204,203,200_.jpg"
-    }
-  ];
+  books: Book[];
   selectedBook: Book;
   // indicate whether the user is creating a new book
   creatingBook: boolean = false;
-  constructor() {}
+  constructor(
+    private booksService: BooksService,
+    private jwtAuth: JWTAuthService
+  ) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getAllBooks();
+  }
 
   createBookTemplate() {
+    if (!this.jwtAuth.hasJwt()) {
+      alert("Login First!");
+      return;
+    }
     this.selectedBook = {
       author: "",
       content: "",
       date: "",
-      id: null,
+      id: "",
       img: "",
       price: null,
       title: ""
     };
   }
 
-  selectBook(index: number) {
-    this.selectedBook = this.books[index];
+  selectBook(book: Book) {
+    this.selectedBook = book;
   }
 
   updateBook(book: Book) {
-    // PUT request
+    if (!this.jwtAuth.hasJwt()) {
+      alert("Login First!");
+      return;
+    }
+
+    this.booksService.updateBook(book).subscribe({
+      next: (resp: HttpResponse<any>) => {
+        if (resp.status != 200) console.log(`Failed to update "${book.id}"`);
+        else console.log(`Successfully updated "${book.id}"`);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+    // refresh
+    this.getAllBooks();
   }
 
   createBook(book: Book) {
-    // POST request
+    if (!this.jwtAuth.hasJwt()) {
+      alert("Login First!");
+      return;
+    }
+
+    this.booksService.createBook(book).subscribe({
+      next: (resp: HttpResponse<any>) => {
+        if (resp.status != 201) console.log(`Failed to create "${book.id}"`);
+        else console.log(`Successfully created "${book.id}"`);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+    // refresh
+    this.getAllBooks();
   }
 
   deleteBook(book: Book) {
-    // DELETE request
+    if (!this.jwtAuth.hasJwt()) {
+      alert("Login First!");
+      return;
+    }
+
+    this.booksService.deleteBook(book.id).subscribe({
+      next: (resp: HttpResponse<any>) => {
+        if (resp.status != 200) console.log(`Failed to delete "${book.id}"`);
+        else console.log(`Successfully deleted "${book.id}" at "${resp.body}"`);
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+    // refresh
+    this.getAllBooks();
+  }
+
+  getAllBooks() {
+    this.booksService.fetchAllBooks().subscribe((allBooks: Book[]) => {
+      this.books = allBooks;
+    });
   }
 }
