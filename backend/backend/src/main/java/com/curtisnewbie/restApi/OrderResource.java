954acce45cf1802fee8d9acd1138d59b48eaee61
CurtisNewbie <zhuangyongj@gmail.com>
2020-02-19 19:20:24 +0000

    Add REST endpoint and repository to update order

--- backend/backend/src/main/java/com/curtisnewbie/restApi/OrderResource.java
+++ backend/backend/src/main/java/com/curtisnewbie/restApi/OrderResource.java
@@ -8,6 +8,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -90,5 +91,14 @@ public class OrderResource {
             return Response.ok().build();
         else
             return Response.noContent().build();
+    }
+
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @RolesAllowed(SecurityRole.ADMIN)
+    public Response updateOrder(Order order) {
+        order = orderRepo.updateOrder(order);
+        return Response.ok(order).build();
     }
 }
