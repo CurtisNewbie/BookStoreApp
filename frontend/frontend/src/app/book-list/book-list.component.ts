0bfd8ef46d4177a0d3cdaeefb309811c18107295
CurtisNewbie <zhuangyongj@gmail.com>
2020-06-19 22:56:13 +0800

    Refactor book-list.component.ts

--- frontend/frontend/src/app/book-list/book-list.component.ts
+++ frontend/frontend/src/app/book-list/book-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { Book, BEBook } from "../model/book";
+import { Book, BEBook, toBook } from "../model/book";
 import { FetchBookService } from "../fetch-book.service";
 import { HttpErrorResponse } from "@angular/common/http";
 
@@ -11,35 +11,21 @@ import { HttpErrorResponse } from "@angular/common/http";
 export class BookListComponent implements OnInit {
   books: Book[] = [];
 
-  constructor(private fetchBookService: FetchBookService) {}
+  constructor(private fetchBookService: FetchBookService) { }
 
   ngOnInit() {
+    // fetch all books
     this.fetchBooks();
   }
 
   /** Fetch all books from backend */
-  fetchBooks() {
+  private fetchBooks(): void {
     this.fetchBookService.fetchBooks().subscribe(
       (books: BEBook[]) => {
         console.log(`Fetched ${books.length} books in total`);
         for (let b of books) {
           // push Book object
-          this.books.push({
-            id: b.id,
-            title: b.title,
-            author: b.author,
-            content: b.content,
-            price: b.price,
-            date:
-              b.date !== undefined
-                ? new Date(
-                    parseInt(b.date.substring(0, 4)),
-                    parseInt(b.date.substring(5, 7)) - 1,
-                    parseInt(b.date.substring(8, 10))
-                  )
-                : null,
-            img: b.img !== undefined ? b.img : null
-          });
+          this.books.push(toBook(b));
         }
       },
       (error: HttpErrorResponse) => {
